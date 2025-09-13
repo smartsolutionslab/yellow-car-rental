bcec4fdb794b943fd9a79f6dddb38c7e8f27f9f7
Heko Weiss <git@smartsolutionslab.tech>
2025-09-13 13:40:51 +0200

    start with domain model

---
+++ src/YellowCarRental.Domain/Booking.cs
@@ -0,0 +1,23 @@
+﻿namespace SmartSolutionsLab.YellowCarRental.Domain;
+
+public class Booking : IRootEntity
+{
+    public Guid Id { get; private set; }
+    public Guid VehicleId { get; private set; }
+    public Guid CustomerId { get; private set; }
+    public DateRange Period { get; private set; }
+    public decimal TotalPrice { get; private set; }
+
+    private Booking() { } // EF Core
+
+    public Booking(Guid vehicleId, Guid customerId, DateRange period, decimal pricePerDay)
+    {
+        Id = Guid.NewGuid();
+        VehicleId = vehicleId;
+        CustomerId = customerId;
+        Period = period;
+
+        var days = period.TotalDaysInclusive();
+        TotalPrice = days * pricePerDay;
+    }
+}
---
+++ src/YellowCarRental.Domain/DateRange.cs
@@ -0,0 +1,20 @@
+﻿namespace SmartSolutionsLab.YellowCarRental.Domain;
+
+public record DateRange(DateOnly Start, DateOnly End) : IValueObject
+{
+    public static DateRange Create(DateOnly start, DateOnly end)
+    {
+        if (end < start) throw new ArgumentException("End before start");
+        if (start < DateOnly.FromDateTime(DateTime.UtcNow.Date)) throw new ArgumentException("Start in the past");
+        return new DateRange(start, end);
+    }
+
+    public int TotalDaysInclusive()
+    {
+        // incl. last day (z.B. 01–05 => 5 days)
+        return (End.ToDateTime(TimeOnly.MinValue) - Start.ToDateTime(TimeOnly.MinValue)).Days + 1;
+    }
+
+    public bool Overlaps(DateRange other) =>
+        !(End < other.Start || Start > other.End);
+}
